4994509425565c85dadd7bee8577a29132d04e1a
Rohan <rohansharma.08062001@gmail.com>
2020-08-11 16:39:07 +0530

    create sign-in sign-out controller (signout with the help of clearCookie() add restrictions to profile page with the help of cookies)

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,8 +1,21 @@
 const User = require('../models/user');
+const { readyState } = require('../config/mongoose');
 module.exports.profile = function(req, res){
-    return res.render('users', {
-        title: 'USER'
-    });
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id, function(err, user){
+            if(user){
+                console.log(user);
+                return res.render('users', {
+                    title: 'USER',
+                    user:user
+                });
+            }
+            return res.redirect('/users/sign-in')
+        })
+    }
+    else{
+        return res.redirect('/users/sign-in');
+    }
 }
 module.exports.signin = function(req, res){
     return res.render('sign-in.ejs',{
@@ -42,5 +55,23 @@ module.exports.create = function(req, res){
     });
 }
 module.exports.createSession = function(req, res){
-    // todo later
+    console.log(req.body);
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('error in finding user in signing up',err); return}
+        if(user){
+            console.log(user.password, req.body.password);
+            if(user.password != req.body.password){
+                return res.redirect('back');
+            }
+            res.cookie('user_id', user.id);
+            return res.redirect('/users/profile');
+        }
+        else{
+            return res.redirect('back');
+        }
+    });
+}
+module.exports.signOut = function(req, res){
+    res.clearCookie('user_id');
+    res.redirect('back');
 }
