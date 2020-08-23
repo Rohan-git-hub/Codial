8fabc430c094693c94591327969ac36425ae9394
Rohan <rohansharma.08062001@gmail.com>
2020-08-23 22:48:11 +0530

    fix a bug

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -10,40 +10,57 @@
                 success: function(data) {
                     let newPost = newPostDom(data.data.post);
                     $('#post-list-container>ul').prepend(newPost);
+                    deletePost($(' .delete-post-button', newPost));
                 },
                 error: function(error) {
-                    console.log(error.responseTeaxt);
+                    console.log(error.responseText);
                 }
             });
         });
     }
-    let newPostDom = function(post) {
+    let newPostDom = function(post){
         return $(`<li id="post-${post._id}">
-            <p>
-                    
-                    
-                    <small>
-                            <a href="/posts/destroy/${post._id}" class="delete-post-button">X</a>
-                    </small>
-                    ${post.content}
-                    <br>
-                    <small>
-                            ${post.user.name}
-                    </small>
-            </p> 
-            <div class="post-comments">
-                            <form action="/comments/create" method="POST">
-                            <input type="text" name="content" placeholder="type here to comment">
-                            <input type="hidden" name="post" value="${post.id}">
-                            <input type="submit" value="add comment">
-                    </form>    
-                    <div class="post-comments-list">
-                            <ul id="post-comments-${post.id}">
-                            </ul>
-                    </div>
-            </div>  
-        </li>`)
+        <p>     
+                <small>
+                        <a href="/posts/destroy/${post._id}" class="delete-post-button">X</a>
+                </small>
+                ${post.content}
+                <br>
+                <small>
+                ${post.user.name}
+                </small>
+        </p> 
+        <div class="post-comments">
+                        <form action="/comments/create" method="POST">
+                        <input type="text" name="content" placeholder="type here to comment">
+                        <input type="hidden" name="post" value="${post._id}">
+                        <input type="submit" value="add comment">
+                </form>    
+                <div class="post-comments-list">
+                        <ul id="post-comments-${post._id}">
+                        </ul>
+                </div>
+        </div>  
+    </li>`)
     }
+    let deletePost = function(deleteLink){
+        $(deleteLink).click(function(e){
+            e.preventDefault();
+
+            $.ajax({
+                type: 'get',
+                url: $(deleteLink).prop('href'),
+                success: function(data){
+                    $(`#post-${data.data.post_id}`).remove();
+                },error: function(error){
+                    console.log(error.responseText);
+                }
+            });
+
+        });
+    }
+
+
 
     createPost();
 }
