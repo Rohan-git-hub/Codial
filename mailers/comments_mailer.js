6350f5fde4df0a6af739394b37ed1319dd2948c6
Rohan <rohansharma.08062001@gmail.com>
2020-08-25 14:49:46 +0530

    set up nodemailer for rendering templates and send a tempelated email

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -1,11 +1,12 @@
 const nodeMailer = require('../config/nodemailer');
 exports.newComment = (comment) => {
     console.log('inside newComment mailer');
+    let htmlString = nodeMailer.renderTempelate({comment: comment}, '/comments/new_comment.ejs');
     nodeMailer.transporter.sendMail({
         from: 'rohan@codeial.com',
         to: comment.user.email,
         subject: 'New Comment Published',
-        html: '<h1>Yup, Your Comment Posted</h1>'
+        html: htmlString
     }, (err, info) => {
         if(err){console.log('error in sending the mail', err); return;}
         console.log('Mail Dilevered');
